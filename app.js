ccde6a7f6b02f59e5cce78e7b11187cc905ea7bc
Alexandre Meurot <alexandre.meurot@tessi.fr>
2022-05-05 16:51:42 +0200

    Import des routes utilisateur dans l'application

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
+const userRoutes = require('./routes/user');
+
 const app = express();
 
 mongoose
@@ -24,6 +26,8 @@ app.use((req, res, next) => {
         "GET, POST, PUT, DELETE, PATCH, OPTIONS"
     );
     next();
-})
+});
+
+app.use('/api/auth', userRoutes);
 
 module.exports = app;
