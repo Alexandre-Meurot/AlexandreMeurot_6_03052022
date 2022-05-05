b99d257a9f2b8da913e11bc81e008e5d68724bf3
Alexandre Meurot <alexandre.meurot@tessi.fr>
2022-05-05 16:37:23 +0200

    Ajout du controller utilisateur avec bcrypt et jsonwebtoken

--- models/User.js
+++ models/User.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const uniqueValidator = require('mongoose-unique-validator');
 
+// modèle mongoose Utilisateur
 const userSchema = mongoose.Schema({
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
