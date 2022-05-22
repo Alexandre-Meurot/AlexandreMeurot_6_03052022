a5f34602fa801b0dffeda7523b38ed314b7a9273
Alexandre Meurot <meurotalexandre@yahoo.fr>
2022-05-22 15:57:00 +0200

    Ajout middleware de config multer concernant l'ajout d'images

--- routes/sauce.js
+++ routes/sauce.js
@@ -8,6 +8,6 @@ router.post('/', auth, sauceCtrl.createSauce);
 router.put('/:id', auth, sauceCtrl.modifySauce);
 router.delete('/:id', auth, sauceCtrl.deleteSauce);
 router.get('/:id', auth, sauceCtrl.getOneSauce);
-router.get('', auth,  sauceCtrl.getAllSauces);
+router.get('', auth, sauceCtrl.getAllSauces);
 
 module.exports = router;
