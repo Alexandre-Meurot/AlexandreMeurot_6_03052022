1a37d7ab538d25dfad48c96348c566a340f54b1b
Alexandre Meurot <meurotalexandre@yahoo.fr>
2022-05-22 15:03:41 +0200

    Ajout des controllers sauce

---
+++ controllers/sauce.js
@@ -0,0 +1,56 @@
+const Sauce = require('../models/Sauce');
+
+// méthode de création de sauce
+exports.createSauce = (req, res, next) => {
+    delete req.body._id;
+    const sauce = new Sauce({
+        ...req.body,
+    });
+    sauce
+        .save()
+        .then(() => res.status(201).json({ message: 'Sauce enregistrée !' }))
+        .catch((error) => res.status(400).json({ error }));
+};
+
+// méthode de modification de sauce
+exports.modifySauce = (req, res, next) => {
+    Sauce.updateOne({_id: req.params.id }, { ...req.body, _id: req.params.id })
+        .then(() => res.status(200).json({ message: 'Sauce modifiée !' }))
+        .catch((error) => res.status(400).json({error}));
+};
+
+// méthode de suppression de sauce
+exports.deleteSauce = (req, res, next) => {
+    Sauce.findOne({ _id: req.params.id }).then((sauce) => {
+        // vérifie si la sauce existe
+        if (!sauce) {
+            return res.status(404).json({
+                error: new Error('Sauce non trouvée !')
+            });
+        }
+        // vérifie si le user est différent du user qui a créée la sauce 
+        if (sauce.userId !== req.auth.userId) {
+            return res.status(401).json({
+                error: new Error('Requête non autorisée !'),
+            });
+        }
+        // Si pas dans les deux cas ci-dessus = suprimme la sauce
+        Sauce.deleteOne({ _id: req.params.id })
+        .then(() => res.status(200).json({ message: 'Sauce supprimée !' }))
+        .catch((error) => res.status(400).json({ error }));
+    });
+};
+
+// méthode pour récupérer une sauce
+exports.getOneSauce = (req, res, next) => {
+    Sauce.findOne({ _id: req.params.id })
+        .then((sauce) => res.status(200).json(sauce))
+        .catch((error) => res.status(404).json({ error }));
+};
+
+// méthode pour récupérer toutes les sauces
+exports.getAllSauces = (req, res, next) => {
+    Sauce.find()
+        .then((sauces) => res.status(200).json(sauces))
+        .catch((error) => res.status(400).json({ error }));
+};
