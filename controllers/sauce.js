7d15ca9d6fc6611f27af36c5d8fbd591080a30c8
Alexandre Meurot <meurotalexandre@yahoo.fr>
2022-05-22 17:17:35 +0200

    Modifications controller sauce

--- controllers/sauce.js
+++ controllers/sauce.js
@@ -20,7 +20,7 @@ exports.modifySauce = (req, res, next) => {
       ...JSON.parse(req.body.thing),
       imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
     } : { ...req.body };
-    Sauce.updateOne({_id: req.params.id }, { ...req.body, _id: req.params.id })
+    Sauce.updateOne({_id: req.params.id }, { sauceObject, _id: req.params.id })
         .then(() => res.status(200).json({ message: 'Sauce modifiée !' }))
         .catch((error) => res.status(400).json({error}));
 };
