6f5ce220df859086a23a3078ad72b4efbc8e0c63
Alexandre Meurot <meurotalexandre@yahoo.fr>
2022-05-22 16:12:37 +0200

    Ajout du gestionnaire de routage danss app.js

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 
 const userRoutes = require('./routes/user');
 const sauceRoutes = require('./routes/sauce');
+const path = require('path')
 
 const app = express();
 
@@ -29,6 +30,7 @@ app.use((req, res, next) => {
     next();
 });
 
+app.use('/images', express.static(path.join(__dirname, 'images')));
 app.use('/api/sauces', sauceRoutes);
 app.use('/api/auth', userRoutes);
 
