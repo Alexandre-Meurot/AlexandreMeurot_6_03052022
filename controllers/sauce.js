8f0b4efea3b3a2d7c825a2eab44804722bcd0567
Alexandre Meurot <meurotalexandre@yahoo.fr>
2022-05-23 15:44:09 +0200

    Correction moddèle Sauce et méthode de suppression de sauce

--- controllers/sauce.js
+++ controllers/sauce.js
@@ -1,4 +1,5 @@
 const Sauce = require('../models/Sauce');
+const fs = require('fs');
 
 // méthode de création de sauce
 exports.createSauce = (req, res, next) => {
@@ -6,10 +7,6 @@ exports.createSauce = (req, res, next) => {
     delete sauceObject._id;
     const sauce = new Sauce({
         ...sauceObject,
-        likes: 0,
-        dislikes: 0,
-        usersLiked: [],
-        usersDisliked: [],
         imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
     });
     sauce.save()
@@ -29,27 +26,18 @@ exports.modifySauce = (req, res, next) => {
         .catch((error) => res.status(400).json({error}));
 };
 
-// méthode de suppression de sauce
+// méthode de suppression de sauce et de son image
 exports.deleteSauce = (req, res, next) => {
     Sauce.findOne({ _id: req.params.id })
-        .then((sauce) => {
-        // vérifie si la sauce existe
-        if (!sauce) {
-            return res.status(404).json({
-                error: new Error('Sauce non trouvée !')
-            });
-        }
-        // vérifie si le user est différent du user qui a créée la sauce 
-        if (sauce.userId !== req.auth.userId) {
-            return res.status(401).json({
-                error: new Error('Requête non autorisée !'),
-            });
-        }
-        // Si pas dans les deux cas ci-dessus = suprimme la sauce
-        Sauce.deleteOne({ _id: req.params.id })
-        .then(() => res.status(200).json({ message: 'Sauce supprimée !' }))
-        .catch((error) => res.status(400).json({ error }));
-    });
+        .then(sauce => {
+            const filename = sauce.imageUrl.split('/images/')[1];
+            fs.unlink(`images/${filename}`, () => {
+            Sauce.deleteOne({ _id: req.params.id })
+                .then(() => res.status(200).json({ message: 'Sauce supprimée !' }))
+                .catch((error) => res.status(400).json({ error }));
+            })
+        })
+        .catch(error => res.status(500).json({ error }));
 };
 
 // méthode pour récupérer une sauce
