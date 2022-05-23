e44bcced3e313b6665aff8997c00565ded0d7096
Alexandre Meurot <meurotalexandre@yahoo.fr>
2022-05-23 17:29:33 +0200

    Correction controller modification d'une sauce

--- controllers/sauce.js
+++ controllers/sauce.js
@@ -27,7 +27,7 @@ exports.createSauce = (req, res, next) => {
 exports.modifySauce = (req, res, next) => {
   const sauceObject = req.file
     ? {
-        ...JSON.parse(req.body.thing),
+        ...JSON.parse(req.body.sauce),
         imageUrl: `${req.protocol}://${req.get("host")}/images/${
           req.file.filename
         }`,
